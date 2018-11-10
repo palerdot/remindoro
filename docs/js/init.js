dbdf30f1a861735d96ce8eee1d8dda503e2c14d5
palerdot <palerdot@gmail.com>
2018-11-10 21:52:10 +0530

    [site] moved source code of site inside docs

---
+++ docs/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    // init scrollspy
+    $(".scrollspy").scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
