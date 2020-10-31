eb9432d8195a963d84d395fab766aab24e040124
palerdot <palerdot@gmail.com>
2020-10-31 12:41:45 +0530

    refactor: css baseline

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 // main app css
 import './css/index.css'
 
 function App() {
-  return <div>porumai ... wait and hope</div>
+  return (
+    <div>
+      <CssBaseline />
+      <div>{'porumai ... wait and hope'}</div>
+    </div>
+  )
 }
 
 export default App
