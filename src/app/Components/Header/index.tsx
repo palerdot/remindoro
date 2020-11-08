77a2a2b4ea5b4682c7450644939f90caad43a079
palerdot <palerdot@gmail.com>
2020-11-08 15:34:07 +0530

    progress: basic drawer menu

--- src/app/Components/Header/index.tsx
+++ src/app/Components/Header/index.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Drawer from '@material-ui/core/Drawer'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const Holder = styled.div`
   display: flex;
 `
 
+const MenuButton = styled(IconButton)`
+  color: white !important;
+`
+
 function Header() {
-  return <Holder>{'porumai ... wait and hope !!!'}</Holder>
+  const [isMenuOpen, setMenuStatus] = useState(false)
+
+  return (
+    <Holder>
+      <MenuButton onClick={() => setMenuStatus(true)}>
+        <MenuIcon fontSize={'large'} />
+      </MenuButton>
+      <Drawer
+        anchor={'left'}
+        open={isMenuOpen}
+        onClose={() => setMenuStatus(false)}
+      >
+        {'porumai ... wait and hope ... '}
+      </Drawer>
+    </Holder>
+  )
 }
 
 export default Header
