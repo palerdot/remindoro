ffb85aa0a073d62102ffc1f1e335213427c69180
palerdot <palerdot@gmail.com>
2021-08-23 22:59:18 +0530

    progress: better newline handling

--- src/app/Components/LiveNote/utils.ts
+++ src/app/Components/LiveNote/utils.ts
@@ -23,11 +23,30 @@ export function transformNewLines(children: Array<LeafNode>): Array<PNode> {
   const transformed: Array<PNode> = []
 
   children.forEach(mark => {
+    console.log('porumai .... handling new line split ', mark)
     const splitted = mark.text.split('\n')
+    // IMPORTANT
+    // Interesting edge case
+    // Each p/paragraph already has a \n (newline) ending
+    // we cannot be splitting that and making again a new p tag
+    // that will cyclically increase the new lines
+    // so we need to deliberatly ignore the ending newline
+    const TOTAL_NEWLINES_TO_IGNORE = 2
+    let ENDING_NEWLINE_IGNORED = 0
+
     splitted.forEach(s => {
       // if empty we will push empty p tag
       const isEmpty = s.trim() === ''
       if (isEmpty) {
+        // but before that
+        // we will ignore the ending new line
+        if (ENDING_NEWLINE_IGNORED < TOTAL_NEWLINES_TO_IGNORE) {
+          // ignore ending new line
+          ENDING_NEWLINE_IGNORED = ENDING_NEWLINE_IGNORED + 1
+          // do not proceed to enter an empty p tag
+          return
+        }
+
         transformed.push({
           type: 'p',
           children: [{ text: '' }],
