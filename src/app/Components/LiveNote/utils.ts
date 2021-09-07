e0cf00073bfd2c9a4f921179629f51c8b034e16f
palerdot <palerdot@gmail.com>
2021-09-07 21:20:44 +0530

    progress: better new line handling with paragraph chunking

--- src/app/Components/LiveNote/utils.ts
+++ src/app/Components/LiveNote/utils.ts
@@ -1,5 +1,5 @@
 import { LeafNode, SlateNode, isLeafNode } from 'slate-mark'
-import { compact, head, tail, isEmpty, findIndex, slice } from '@lodash'
+import { compact, last, isEmpty, findIndex, slice } from '@lodash'
 import styled, { css } from 'styled-components'
 
 export const LiveNoteStyles = css`
@@ -42,115 +42,122 @@ export function transformNewLines(children: Array<LeafNode>): Array<PNode> {
   const transformed: Array<PNode> = []
 
   children.forEach((mark, index, origChildren) => {
-    // track ignore count
-    let EMPTY_LINES_IGNORE_COUNT = 1
-    let NON_EMPTY_LINES_IGNORE_COUNT = 0
+    let originalSplitted = mark.text.split(`${NEWLINE_MAGIC_TOKEN}\n`)
 
-    const splitted = mark.text.split(`${NEWLINE_MAGIC_TOKEN}\n`)
-
-    // todo: multi split
-    // input => ["porumai  ", "", "", "patience ", "", "", "amaidhi ", "", ""]
-    // output => [ ["porumai  ", "", ""], ["patience ", "", ""], ["amaidhi ", "", ""] ]
-
-    // edge case:
-    // we may deal with only empty lines or content + empty lines
-    // if content + empty lines; we may have to ignore new lines (see below)
-    const onlyEmptyLines =
-      splitted.join('').replaceAll(NEWLINE_MAGIC_TOKEN, '').trim() === ''
-
-    if (onlyEmptyLines) {
-      EMPTY_LINES_IGNORE_COUNT = 1
-    } else {
-      // edge case
-      // if it is a line with first element as non empty
-      // and rest of the lines are empty
-      const first = head(splitted) || ''
-      const rest = tail(splitted)
-
-      if (first.endsWith(' ') && isEmpty(compact(rest))) {
-        NON_EMPTY_LINES_IGNORE_COUNT = 1
-      }
+    // trim last space
+    if (isEmpty(last(originalSplitted))) {
+      originalSplitted = originalSplitted.slice(0, -1)
     }
 
-    const hasEmptySpaces = onlyEmptyLines && compact(splitted).length > 0
+    const multiLineSplit = chunkParagraphs(originalSplitted)
 
-    if (hasEmptySpaces) {
-      EMPTY_LINES_IGNORE_COUNT = 0
+    multiLineSplit.forEach(splitted => {
+      // track ignore count
+      let EMPTY_LINES_IGNORE_COUNT = 1
+      let NON_EMPTY_LINES_IGNORE_COUNT = 0
 
-      // one more edge case; we need to check if previous line is a mark
-      const prevLine = origChildren[index - 1]
+      // edge case:
+      // we may deal with only empty lines or content + empty lines
+      // if content + empty lines; we may have to ignore new lines (see below)
+      const onlyEmptyLines =
+        splitted.join('').replaceAll(NEWLINE_MAGIC_TOKEN, '').trim() === ''
 
-      if (prevLine) {
-        const isBold = prevLine.bold === true
-        const isItalic = prevLine.italic === true
-        const isStrikeThrough = prevLine.strikethrough === true
-        const isInlineCode = prevLine.code === true
-        const isPrevLineMark =
-          isBold || isItalic || isStrikeThrough || isInlineCode
+      if (onlyEmptyLines) {
+        EMPTY_LINES_IGNORE_COUNT = 1
+      }
+      /* else {
+        // edge case
+        // if it is a line with first element as non empty
+        // and rest of the lines are empty
+        const first = head(splitted) || ''
+        const rest = tail(splitted)
 
-        if (isPrevLineMark) {
-          EMPTY_LINES_IGNORE_COUNT = 1
+        if (first.endsWith(' ') && isEmpty(compact(rest))) {
+          NON_EMPTY_LINES_IGNORE_COUNT = 1
+        }
+      } */
+
+      const hasEmptySpaces = onlyEmptyLines && compact(splitted).length > 0
+
+      if (hasEmptySpaces) {
+        EMPTY_LINES_IGNORE_COUNT = 0
+
+        // one more edge case; we need to check if previous line is a mark
+        const prevLine = origChildren[index - 1]
+
+        if (prevLine) {
+          const isBold = prevLine.bold === true
+          const isItalic = prevLine.italic === true
+          const isStrikeThrough = prevLine.strikethrough === true
+          const isInlineCode = prevLine.code === true
+          const isPrevLineMark =
+            isBold || isItalic || isStrikeThrough || isInlineCode
+
+          if (isPrevLineMark) {
+            EMPTY_LINES_IGNORE_COUNT = 1
+          }
         }
       }
-    }
 
-    // IMPORTANT
-    // Interesting edge case
-    // Each p/paragraph already has a \n (newline) ending
-    // we cannot be splitting that and making again a new p tag
-    // that will cyclically increase the new lines
-    // so we need to deliberatly ignore the ending newline
+      // IMPORTANT
+      // Interesting edge case
+      // Each p/paragraph already has a \n (newline) ending
+      // we cannot be splitting that and making again a new p tag
+      // that will cyclically increase the new lines
+      // so we need to deliberatly ignore the ending newline
 
-    const TOTAL_NEWLINES_TO_IGNORE = onlyEmptyLines
-      ? EMPTY_LINES_IGNORE_COUNT
-      : // : 1
-        NON_EMPTY_LINES_IGNORE_COUNT // 0
-    let ENDING_NEWLINE_IGNORED = 0
+      const TOTAL_NEWLINES_TO_IGNORE = onlyEmptyLines
+        ? EMPTY_LINES_IGNORE_COUNT
+        : // : 1
+          NON_EMPTY_LINES_IGNORE_COUNT // 0
+      let ENDING_NEWLINE_IGNORED = 0
 
-    console.log(
-      'porumai ... new line split ',
-      splitted,
-      TOTAL_NEWLINES_TO_IGNORE,
-      children
-    )
+      console.log(
+        'porumai ... new line split ',
+        splitted,
+        TOTAL_NEWLINES_TO_IGNORE,
+        children,
+        originalSplitted
+      )
 
-    splitted.forEach(s => {
-      const text = s.replaceAll(NEWLINE_MAGIC_TOKEN, '')
+      splitted.forEach(s => {
+        const text = s.replaceAll(NEWLINE_MAGIC_TOKEN, '')
 
-      // if empty we will push empty p tag
-      const isEmpty = text.trim() === ''
-      if (isEmpty) {
-        // we will ignore empty spaces ??? ' '
-        /* if (text !== '') {
-          return
-        } */
+        // if empty we will push empty p tag
+        const isEmpty = text.trim() === ''
+        if (isEmpty) {
+          // we will ignore empty spaces ??? ' '
+          /* if (text !== '') {
+        return
+      } */
 
-        // but before that
-        // we will ignore the ending new line
-        if (ENDING_NEWLINE_IGNORED < TOTAL_NEWLINES_TO_IGNORE) {
-          // ignore ending new line
-          ENDING_NEWLINE_IGNORED = ENDING_NEWLINE_IGNORED + 1
-          // do not proceed to enter an empty p tag
-          return
+          // but before that
+          // we will ignore the ending new line
+          if (ENDING_NEWLINE_IGNORED < TOTAL_NEWLINES_TO_IGNORE) {
+            // ignore ending new line
+            ENDING_NEWLINE_IGNORED = ENDING_NEWLINE_IGNORED + 1
+            // do not proceed to enter an empty p tag
+            return
+          }
+
+          transformed.push({
+            type: 'p',
+            children: [{ text: '' }],
+          })
+        } else {
+          // we will push the text with marks (bold, italic etc)
+          // let us replace our new line token
+          transformed.push({
+            type: 'p',
+            children: [
+              {
+                ...mark,
+                text,
+              },
+            ],
+          })
         }
-
-        transformed.push({
-          type: 'p',
-          children: [{ text: '' }],
-        })
-      } else {
-        // we will push the text with marks (bold, italic etc)
-        // let us replace our new line token
-        transformed.push({
-          type: 'p',
-          children: [
-            {
-              ...mark,
-              text,
-            },
-          ],
-        })
-      }
+      })
     })
   })
 
