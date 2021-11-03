a31873c8ba5fad1672462d2dbd3c5033a46f6641
palerdot <palerdot@gmail.com>
2021-11-03 10:02:54 +0530

    scripts: firefox generate script fix

--- generate-firefox-source.sh
+++ generate-firefox-source.sh
@@ -4,13 +4,15 @@
 #  - `yarn` (or `yarn install`). This step will install all necessary dependencies
 # 
 # Build steps:
-#  - `gulp build-firefox-remindoro`. 
+#  - `yarn build:firefox`. 
 # 
-#  This gulp task will build the final archive from source. The location of final archive is `dist/firefox`. The zip file is the final file uploaded to add-on store. Rest of the files are the contents of the zip file.
+#  This yarn task will build the final archive from source. The location of final archive is `release/firefox`. The zip file is the final file uploaded to add-on store. Rest of the files are the contents of the zip file.
 # 
 #  Environment Info:
-#   - node `v10.15.2`
+#   - node `v12.x`
 #   - npm `v6.4.1`
 #   - OS `Mac OS 10.14.2`
 
-tar --exclude='./node_modules' --exclude='./release' --exclude='./dev-server'  --exclude='./build' --exclude='./dist' --exclude='./docs' --exclude='.git' -cvzf ~/Desktop/remindoro-firefox-source.tar.gz .
+# tar --exclude='./node_modules' --exclude='./screenshots' --exclude='./release' --exclude='./dev-server'  --exclude='./build' --exclude='./dist' --exclude='./docs' --exclude='.git' -cvzf ~/Desktop/remindoro-firefox-source.tar.gz .
+
+tar -cvzf ~/Desktop/remindoro-firefox-source.tar.gz package.json tsconfig.json yarn.lock src/
