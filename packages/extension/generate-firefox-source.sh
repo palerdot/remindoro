ebb2144f2d101b057a0b052ed495e4412196985b
palerdot <palerdot@gmail.com>
2023-11-02 11:28:00 +0530

    firefox source generation script changes

--- packages/extension/generate-firefox-source.sh
+++ packages/extension/generate-firefox-source.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 # Installation Steps:
-#  - `yarn` (or `yarn install`). This step will install all necessary dependencies. Yarn v1 is used for this project.
+#  - `pnpm install` (or `pnpm install`). This step will install all necessary dependencies. pnpm v8 is used for this project.
 # 
 # Build steps:
-#  - `yarn build:firefox`. 
+#  - `pnpm build:firefox`. 
 # 
 #  This yarn task will build the final archive from source. The location of final archive is `release/firefox`. The zip file is the final file uploaded to add-on store. Rest of the files are the contents of the zip file.
 # 
 #  Environment Info:
 #   - node `v16.x`
-#   - npm `v8.x` / yarn `v1.22.x`
+#   - npm `v8.x` / pnpm 8.x
 #   - OS `Mac OS 12.6.3`
 
-tar -cvzf ~/Desktop/remindoro-firefox-source.tar.gz package.json tsconfig.json yarn.lock src/
+tar -cvzf ~/Desktop/remindoro-firefox-source.tar.gz package.json tsconfig.json pnpm-lock.yaml src/
