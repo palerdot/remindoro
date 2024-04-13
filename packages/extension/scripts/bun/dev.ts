a2553e1b5c7483b7cf6bda91a282de34595e6672
palerdot <palerdot@gmail.com>
2024-04-13 20:26:45 +0530

    basic hot reloading with file watcher

--- packages/extension/scripts/bun/dev.ts
+++ packages/extension/scripts/bun/dev.ts
@@ -1,15 +1,35 @@
 process.env.NODE_ENV = 'development'
 
+import { watch } from 'fs'
 import fs from 'node:fs/promises'
-import { last } from 'lodash'
+import { head, last } from 'lodash'
 import yargs from 'yargs'
 import { execa } from 'execa'
-import webExt from 'web-ext'
 import { hideBin } from 'yargs/helpers'
 import stripJsonComments from 'strip-json-comments'
 import 'colors'
 
 const argv = yargs(hideBin(process.argv)).parseSync()
+const browser = argv.browser
+
+const watcher = watch('./src/', { recursive: true }, (event, filename) => {
+  const allowed_folders = ['app', 'background']
+  const root_folder = head(filename?.split('/'))
+  const is_valid_folder =
+    root_folder !== undefined && allowed_folders.includes(root_folder)
+  if (is_valid_folder) {
+    console.log(`Detected ${event} in ${filename}`)
+    build()
+  }
+})
+
+process.on('SIGINT', () => {
+  // close watcher when Ctrl-C is pressed
+  console.log('Closing watcher...')
+  watcher.close()
+
+  process.exit(0)
+})
 
 console.log(
   '\n-----------------------------------------------------\n'.yellow.bold
@@ -20,24 +40,33 @@ const OUT_DIR = 'out'
 start()
 
 function start() {
-  fs.rm(`./${OUT_DIR}`, { force: true }).then(() => {
-    console.log(`porumai ... ${OUT_DIR} deleted `.yellow)
-    build()
-  })
+  fs.rm(`./${OUT_DIR}`, { force: true })
+    .then(() => {
+      console.log(`porumai ... ${OUT_DIR} deleted `.yellow)
+      return build()
+    })
+    .then(browser => {
+      run_local_extension(browser as string)
+    })
 }
 
 function build() {
-  Bun.build({
+  return Bun.build({
     entrypoints: ['./src/init.tsx', './src/background/index.js'],
     outdir: `./${OUT_DIR}`,
     // splitting: true,
-    naming: '[dir]/[name].[ext]',
+    // naming: '[dir]/[name].[ext]',
+    naming: {
+      // default values
+      entry: '[dir]/[name].[ext]',
+      chunk: '[name].[ext]',
+      asset: '[name].[ext]',
+    },
     define: {
       'process.env.BUN_RATE_URL': 'https://jublime.com',
     },
   })
     .then(result => {
-      const browser = argv.browser
       console.log(browser)
       // const chunks: Array<string> = []
       const styles: Array<string> = []
@@ -61,31 +90,34 @@ function build() {
       write_html_file(styles)
       write_manifest_file(browser)
       console.log('porumai ... bun buld finished ?'.blue)
-
-      if (browser === 'firefox') {
-        console.log('porumai ... opening firefox extension')
-
-        execa('web-ext', [
-          'run',
-          '--firefox',
-          '/Applications/Firefox.app/Contents/MacOS/firefox',
-          '--source-dir',
-          'out',
-          '--pref',
-          'startup.homepage_welcome_url=https://www.youtube.com',
-        ])
-          .then(result => {
-            console.log(result.stdout)
-            console.log('porumai ... build finished ? all fine?')
-          })
-          .catch(err => {
-            console.error(err)
-          })
-      }
+      return new Promise(resolve => resolve(browser))
     })
     .catch(error => {
       console.error('porumai ... bun build error'.red)
     })
+}
+
+function run_local_extension(browser: string) {
+  if (browser === 'firefox') {
+    console.log('porumai ... opening firefox extension')
+
+    execa('web-ext', [
+      'run',
+      '--firefox',
+      '/Applications/Firefox.app/Contents/MacOS/firefox',
+      '--source-dir',
+      'out',
+      '--pref',
+      'startup.homepage_welcome_url=https://www.youtube.com',
+    ])
+      .then(result => {
+        console.log(result.stdout)
+        console.log('porumai ... build finished ? all fine?')
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
 }
 
 function write_html_file(styles: Array<string>) {
