38a82b76d41c6a28601d7572c3bd1b3ce9ab8e66
palerdot <palerdot@gmail.com>
2025-12-06 11:08:58 +0530

    chore: update firefox build instructions

--- packages/extension/generate-firefox-source.sh
+++ packages/extension/generate-firefox-source.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 
 # Installation Steps:
-#  - `pnpm install` (or `pnpm install`). This step will install all necessary dependencies. pnpm v8 is used for this project.
-# 
+#  - `pnpm install` (or `pnpm install`). This step will install all necessary dependencies. pnpm v10 is used for this project.
+#
 # Build steps:
-#  - `pnpm build:firefox`. 
-# 
+#  - `pnpm build:firefox`.
+#
 #  This pnpm task will build the final archive from source. The location of final archive is `release/firefox`. The zip file is the final file uploaded to add-on store. Rest of the files are the contents of the zip file.
-# 
+#
 #  Environment Info:
-#   - bun `v1.1.3`
-#   - node `v20.x`
-#   - npm `v10.x` / pnpm 9.x
+#   - bun `v1.3.3`
+#   - node `v22.x`
+#   - npm `v10.x` / pnpm 10.x
 #   - OS `Mac OS 12.6.3`
 
 tar -cvzf ~/Desktop/remindoro-firefox-source.tar.gz package.json tsconfig.json pnpm-lock.yaml src/
